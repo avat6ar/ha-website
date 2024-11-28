09efcf878735b6b7d81244f93be1ca9ecc49bc0a
abdullah mohamed <abdullah2662888@gmail.com>
2024-11-28 07:30:36 +0200

    fix-error-image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["hatcacademy.com"],
+    domains: ["softcitadel.com"],
+    unoptimized: false
   },
   experimental: {
     missingSuspenseWithCSRBailout: false,
